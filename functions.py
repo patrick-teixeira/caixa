55fab1c892e75817e640576df09171ef641da6bf
Patrick <55304605+patrick-teixeira@users.noreply.github.com>
2023-11-02 12:33:17 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,36 +1,6 @@
 import json
 
-def add_autor(autor, obra):
-    with open('dados.json','r+') as file:
-        file_data = json.load(file)
-        if autor in file_data['autores']:
-            if obra not in file_data["autores"][autor]:
-                print('a')
-                file_data["autores"][autor].append(obra)
-                file.seek(0)
-                json.dump(file_data, file, indent = 4)
-                return
-        else:
-            file_data["autores"][autor] = [obra]
-            file.seek(0)
-            json.dump(file_data, file, indent = 4)
-            print(file_data['autores'])
 
-
-def add_editora(editora, obra):
-    with open('dados.json','r+') as file:
-        file_data = json.load(file)
-        if editora in file_data['editoras']:
-            if obra not in file_data["editoras"][editora]:
-                print('a')
-                file_data["editoras"][editora].append(obra)
-                file.seek(0)
-                json.dump(file_data, file, indent = 4)
-                return
-        else:
-            file_data["editoras"][editora] = [obra]
-            file.seek(0)
-            json.dump(file_data, file, indent = 4)
 
 def add_manga(nome, autor, genero, editora, volume, preco):
     manga_data =   {nome: {
@@ -49,13 +19,6 @@ def add_manga(nome, autor, genero, editora, volume, preco):
         file.seek(0)    
         json.dump(file_data, file, indent = 4)
 
-def ler_autor():
-    with open('dados.json', 'r') as file:
-        data = json.load(file)
-        autores = list(data['autores'].keys())
-        for autor in autores:
-            print(autor)
-
 def mangas_estoque():
     mangas = []
     with open('dados.json', 'r') as file:
@@ -64,12 +27,6 @@ def mangas_estoque():
             mangas.append(list(data['mangas'][i].keys())[0])
         return mangas
 
-def ler_editoras():
-    with open('dados.json', 'r') as file:
-        data = json.load(file)
-        editoras = list(data['editoras'].keys())
-        for i in range (len(editoras)):
-            print(editoras[i])
 
 def pesquisar(manga):
     mangas = []
@@ -97,5 +54,3 @@ def valor_manga(manga):
     if manga in mangas:
         return data['mangas'][mangas.index(manga)][manga]['valor']
     
-if __name__ == '__main__':
-    pass
